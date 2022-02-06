7309a01a6450f557adb1788550f1c493ae6dfaac
amogh chavan <chavanamogh1@gmail.com>
2022-02-07 01:09:21 +0530

    query build passed partial test cases

--- src/utils/query-builder.ts
+++ src/utils/query-builder.ts
@@ -1,4 +1,3 @@
-import { Sequelize } from "sequelize/dist";
 import { RestaurantFiltersDto } from "../api/restaurant/dto/restaurant-filter.dto";
 import { Op } from "sequelize";
 export async function filterQueryBuilder(restaurantFiltersDto: RestaurantFiltersDto) {
@@ -28,11 +27,73 @@ export async function filterQueryBuilder(restaurantFiltersDto: RestaurantFilters
         filterQuery.veg_only = restaurantFiltersDto.veg_only
     }
 
-    const query = { where: { ...filterQuery, [Op.or]: [...costArr, ...cusineArr] } };
+    let query;
+    if ([...costArr, ...cusineArr].length > 0) {
+        query = { where: { ...filterQuery, [Op.or]: [...costArr, ...cusineArr] } };
+    }
+    else {
+        query = { where: { ...filterQuery } };
+    }
 
+    console.log("query", query)
     return query
 }
 
 
 
 // const query = { where: { ...filterQuery, [Op.or]: [{ cost: { [Op.like]: '%MEDIUM%' } }, { cost: { [Op.like]: '%LOW%' } }] } };
+
+//mysql
+
+// SELECT restaurant_id,cusine_types FROM restaurant_database.restaurant WHERE cusine_types LIKE '%item1%' OR cusine_types LIKE '%item2%';
+
+const obj = [
+    {
+        "restaurant_name": "priti hotel",
+        "address": "dombivli west char rasta near gopi mall 421202",
+        "veg_only": false,
+        "cost": "LOW",
+        "cusine_types": ["south indian", "italian", "french", "north indian"],
+        "isOpen": true
+    },
+    {
+        "restaurant_name": "pure veg priti hotel",
+        "address": "dombivli west char rasta near gopi mall 421202",
+        "veg_only": true,
+        "cost": "LOW",
+        "cusine_types": ["south indian", "italian", "french", "north indian"],
+        "isOpen": true
+    },
+    {
+        "restaurant_name": "alfresco",
+        "address": "dombivli east near patkar school",
+        "veg_only": false,
+        "cost": "high",
+        "cusine_types": ["italian", "french"],
+        "isOpen": true
+    },
+    {
+        "restaurant_name": "red chilly",
+        "address": "dombivli west near bagshala ground",
+        "veg_only": false,
+        "cost": "Medium",
+        "cusine_types": ["chinese"],
+        "isOpen": true
+    },
+    {
+        "restaurant_name": "morden cafe",
+        "address": "dombivli east fadke road",
+        "veg_only": true,
+        "cost": "Medium",
+        "cusine_types": ["south indian", "italian", "french", "north indian", "chinese"],
+        "isOpen": true
+    },
+    {
+        "restaurant_name": "youth cafe",
+        "address": "ghatkopar",
+        "veg_only": true,
+        "cost": "Medium",
+        "cusine_types": ["south indian", "north indian", "chinese"],
+        "isOpen": false
+    },
+]
